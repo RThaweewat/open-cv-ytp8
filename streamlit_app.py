91c6e2b4af77607e9e63bf48944eab28547efb3f
Thaweewat <thaweewat.data@gmail.com>
2022-08-16 02:34:57 +0700

    Replaced with local code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
-"""
 import cv2
+import streamlit as st
 import mediapipe as mp
 import numpy as np
 from datetime import datetime
@@ -10,20 +10,22 @@ import time
 from pathlib import Path
 # from playsound import playsound
 import gspread
-import streamlit as st
-
-st.title("Webcam Application")
-option = st.selectbox(
-    'Select Video Capture CH.',
-    (0, 1, 2))
-
-# audio = Path().cwd() / "test_2.wav"
-
-# Times
 now = datetime.now().time()
 now = str(now).replace(".", "_").replace(":", "_")
-run = st.checkbox('Start')
-run_no = str(input())
+
+#run = st.checkbox('Start')
+
+st.title("Webcam Live Feed")
+run_no = st.text_input('Run name')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+
+## Setup mediapipe instance
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+fourcc = cv2.VideoWriter_fourcc(*"MP4V")
+out = cv2.VideoWriter(f"output_{str(now)}.mp4", fourcc, 20.0, (640, 480))
 
 def calculate_angle(a, b, c):
     a = np.array(a)  # First
@@ -40,14 +42,6 @@ def calculate_angle(a, b, c):
 
     return angle
 
-
-# Using NumPy
-dtypes = np.dtype(
-    [("No_Name", str), ("Total", str), ("Sit Total", str), ("Time", str), ("Name", str)]
-)
-
-df = pd.DataFrame(np.empty(0, dtype=dtypes))
-
 # Curl counter variables
 counter = 0
 stage = None
@@ -58,186 +52,176 @@ test = True
 prev_frame_time = 0
 new_frame_time = 0
 sit_count = []
+import os.path
+from os.path import exists as file_exists
 
-FRAME_WINDOW = st.image([])
-cap = cv2.VideoCapture(option)
 
-while run:
-    ## Setup mediapipe instance
-    mp_drawing = mp.solutions.drawing_utils
-    mp_pose = mp.solutions.pose
+# Using NumPy
+if file_exists('data.csv'):
+    df = pd.read_csv('data.csv', index_col=0)
+else:
+    dtypes = np.dtype(
+        [
+            ("No", int),
+            ("Total", str),
+            ("Sit Total", str),
+            ("Time", str),
+            ("Name", str)
+        ]
+    )
+    df = pd.DataFrame(np.empty(0, dtype=dtypes))
 
-    fourcc = cv2.VideoWriter_fourcc(*"MP4V")
-    out = cv2.VideoWriter(f"output_{str(now)}.mp4", fourcc, 20.0, (640, 480))
-    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-        while cap.isOpened():
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while camera.isOpened():
 
-            ret, frame = cap.read()
+        ret, frame = camera.read()
 
-            # Recolor image to RGB
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image.flags.writeable = False
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
 
-            # Make detection
-            results = pose.process(image)
+        # Make detection
+        results = pose.process(image)
 
-            # Recolor back to BGR
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            image_height, image_width, _ = image.shape
-            image = cv2.resize(image, (int(image_width * (480 / image_height)), 480))
+        image_height, image_width, _ = image.shape
+        image = cv2.resize(image, (int(image_width * (480 / image_height)), 480))
 
-            # Extract landmarks
-            try:
-                landmarks = results.pose_landmarks.landmark
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
 
-                # Get coordinates
-                shoulder = [
-                    landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
-                    landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y,
-                ]
-                elbow = [
-                    landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
-                    landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y,
-                ]
-                wrist = [
-                    landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-                    landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y,
-                ]
+            # Get coordinates
+            shoulder = [
+                landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y,
+            ]
+            elbow = [
+                landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+                landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y,
+            ]
+            wrist = [
+                landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y,
+            ]
 
-                # Calculate angle
-                angle = calculate_angle(shoulder, elbow, wrist)
+            # Calculate angle
+            angle = calculate_angle(shoulder, elbow, wrist)
 
-                # Visualize angle
-                cv2.putText(
-                    image,
-                    str(angle),
-                    tuple(np.multiply(elbow, [640, 480]).astype(int)),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5,
-                    (255, 255, 255),
-                    2,
-                    cv2.LINE_AA,
-                )
-
-                # Curl counter logic
-
-                if angle > 30:
-                    stage = "sit"
-                    sit_count.append(1)
-
-                if stage == "sit" and angle < 15:
-                    stage = "stand"
-                    counter = time.time() - start_time
-                    counter = float("{0:.2f}".format(counter))
-                    print(counter, "seconds")
-                    sit_to_stand = float("{0:.2f}".format(counter - time_spare))
-                    print("Sit to stand", sit_to_stand, "seconds")
-                    time_spare = counter
-                    sit_count.append(0)
-
-            except:
-                pass
-
-            # Render curl counter
-            # Setup status box
-            cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
-
-            # Rep data
+            # Visualize angle
             cv2.putText(
                 image,
-                "REPS",
-                (15, 12),
+                str(angle),
+                tuple(np.multiply(elbow, [640, 480]).astype(int)),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 0.5,
-                (0, 0, 0),
-                1,
-                cv2.LINE_AA,
-            )
-            cv2.putText(
-                image,
-                str(counter),
-                (10, 60),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                2,
                 (255, 255, 255),
                 2,
                 cv2.LINE_AA,
             )
 
-            # Stage data
-            cv2.putText(
-                image,
-                "STAGE",
-                (65, 12),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (0, 0, 0),
-                1,
-                cv2.LINE_AA,
-            )
-            cv2.putText(
-                image,
-                stage,
-                (60, 60),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                2,
-                (255, 255, 255),
-                2,
-                cv2.LINE_AA,
-            )
+            # Curl counter logic
 
-            # Render detections
-            mp_drawing.draw_landmarks(
-                image,
-                results.pose_landmarks,
-                mp_pose.POSE_CONNECTIONS,
-                mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-                mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2),
-            )
+            if angle > 30:
+                stage = "sit"
+                sit_count.append(1)
 
-            out.write(image)
-            FRAME_WINDOW.image(image)
+            if stage == "sit" and angle < 15:
+                stage = "stand"
+                counter = time.time() - start_time
+                counter = float("{0:.2f}".format(counter))
+                print(counter, "seconds")
+                sit_to_stand = float("{0:.2f}".format(counter - time_spare))
+                print("Sit to stand", sit_to_stand, "seconds")
+                time_spare = counter
+                sit_count.append(0)
 
-            if cv2.waitKey(10) & 0xFF == ord("q"):
-                total_time = float("{0:.2f}".format(time.time() - start_time))
-                print("Total", total_time, "seconds")
-                break
-            elif sit_count[-2:] == [0, 1]:
-                total_time = float("{0:.2f}".format(time.time() - start_time))
-                print("Total", total_time, "seconds")
+        except:
+            pass
 
-                cap.release()
-                out.release()
-                break
+        # Render curl counter
+        # Setup status box
+        cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
 
-        cap.release()
-        cv2.destroyAllWindows()
+        # Rep data
+        cv2.putText(
+            image,
+            "REPS",
+            (15, 12),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 0, 0),
+            1,
+            cv2.LINE_AA,
+        )
+        cv2.putText(
+            image,
+            str(counter),
+            (10, 60),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            2,
+            (255, 255, 255),
+            2,
+            cv2.LINE_AA,
+        )
+
+        # Stage data
+        cv2.putText(
+            image,
+            "STAGE",
+            (65, 12),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 0, 0),
+            1,
+            cv2.LINE_AA,
+        )
+        cv2.putText(
+            image,
+            stage,
+            (60, 60),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            2,
+            (255, 255, 255),
+            2,
+            cv2.LINE_AA,
+        )
+
+        # Render detections
+        mp_drawing.draw_landmarks(
+            image,
+            results.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+            mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2),
+        )
+
+        out.write(image)
+        FRAME_WINDOW.image(image)
+
+        if cv2.waitKey(10) & 0xFF == ord("q"):
+            total_time = float("{0:.2f}".format(time.time() - start_time))
+            print("Total", total_time, "seconds")
+            break
+        elif sit_count[-2:] == [0, 1]:
+            total_time = float("{0:.2f}".format(time.time() - start_time))
+            print("Total", total_time, "seconds")
+
+            camera.release()
+            out.release()
+            break
+
+st.write('Stopped')
 
 # append dataframe
 df =  df.append({"No_Name": run_no
-	                , 'Name':f'output_{str(now)}_run_no{run_no}.mp4'
-	                , 'Sit Total': sit_to_stand
-	                , 'Total': str(now)
-	                , 'Time': total_time}, ignore_index=True)
+                    , 'Name':f'output_{str(now)}_run_no{run_no}.mp4'
+                    , 'Sit Total': sit_to_stand
+                    , 'Total': str(now)
+                    , 'Time': total_time}, ignore_index=True)
 
-
-print("run complete")
-
-"""
-
-import cv2
-import streamlit as st
-
-st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
-
-while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+st.dataframe(df)
+df.to_csv('data.csv')
